953c1f6c50cdd5d39fb3dfc5b99364384cc311db
Amakoye <charlesamakoye@gmail.com>
2023-02-23 22:38:36 +0300

    project setup

---
+++ src/store/index.ts
@@ -0,0 +1,53 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PersistConfig,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const reducer = combineReducers({
+  //reducers go here
+} as const);
+
+const persistConfig: Omit<PersistConfig<State>, "blacklist" | "whitelist"> &
+  Partial<Record<"blacklist" | "whitelist", (keyof State)[]>> = {
+  key: "root",
+  storage,
+  whitelist: [
+    /* whiltelisted reducers go here */
+  ],
+};
+
+const persistedReducer = persistReducer(persistConfig as any, reducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddlware) =>
+    getDefaultMiddlware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const useDispatch = () => store.dispatch;
+
+const persistor = persistStore(store);
+
+// types below
+export type Dispatch = typeof store.dispatch;
+export type State = ReturnType<typeof reducer>;
+
+declare global {
+  export type State = ReturnType<typeof reducer>;
+}
+
+export { store, useDispatch, persistor };
